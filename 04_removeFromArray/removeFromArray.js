3ac91b904e097616df0925c852a212b08f2b0d0d
Michelle Jazmín Perna <76192627+michellejazmin@users.noreply.github.com>
2022-10-07 20:59:08 -0300

    Exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(originalArray, ...argsToRemove) {
+  // For each argument in argsToRemove
+  for (let i = 0; i < argsToRemove.length; i++)
+  {
+    // For each element in the original array
+    for (let j = 0; j < originalArray.length; j++)
+    {
+      if (argsToRemove[i] === originalArray[j])
+      {
+        originalArray.splice(j, 1);
+      }
+    }
+  }
 
+  return originalArray;
 };
 
 // Do not edit below this line
