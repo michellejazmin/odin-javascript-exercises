c19ce5435a6ebe0ba9231edfeb6bfde6ebb8df23
Michelle Jazmín Perna <76192627+michellejazmin@users.noreply.github.com>
2022-10-07 22:40:46 -0300

    Exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(tempFahrenheit) {
+  let tempCelsius = (tempFahrenheit - 32) * 5 / 9;
+  return parseFloat(tempCelsius.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(tempCelsius) {
+  let tempFahrenheit = tempCelsius * 9 / 5 + 32;
+  return Math.round(tempFahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
