ddf00d58f70fc80f02ab6af3a0db879bb3534209
Michelle Jazmín Perna <76192627+michellejazmin@users.noreply.github.com>
2022-10-07 21:43:06 -0300

    Exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  if (!Number.isInteger(num1) || !Number.isInteger(num2)) return 'ERROR'
+  if (num1 < 0 || num2 < 0) return 'ERROR';
+  
+  let min;
+  let max;
 
+  if (num1 < num2) {
+    min = num1;
+    max = num2;
+  } else {
+    min = num2;
+    max = num1;
+  }
+
+  let result = 0;
+
+  for (let i = min; i <= max; i++) {
+    result += i;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
