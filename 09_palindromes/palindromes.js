b94b8d10435cc3040172ceeadd25a4bc8f0d51b9
Michelle Jazmín Perna <76192627+michellejazmin@users.noreply.github.com>
2022-10-12 20:36:57 -0300

    Exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,23 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  string = string.trim().toLowerCase();
+  const letters = Array.from(string).filter(character => /[a-z]/.test(character));
 
+  const len = letters.length;
+  for (let i = 0; i < len / 2; i++) {
+    if (letters[i] !== letters[len - 1 - i]) return false;
+  }
+  return true;
 };
+
+/* Another way to solve
+
+const palindromes = function (string) {
+  processedString = string.toLowerCase().replace(/[^a-z]/g, "");
+  reversedString = processedString.split("").reverse().join("");
+  return reversedString === string;
+};
+
+*/
 
 // Do not edit below this line
 module.exports = palindromes;
