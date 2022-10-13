48d9ab9bb06ee2b55eaa57da504df9710fbd9872
Michelle Jazmín Perna <76192627+michellejazmin@users.noreply.github.com>
2022-10-12 22:34:47 -0300

    Exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+  let oldestAge = 0;
 
+  let oldestPerson = array.reduce((oldest, person) => {   
+    let age = 0; 
+    if (!(person.yearOfDeath)) {
+      age = new Date().getFullYear() - person.yearOfBirth;
+    } else {
+      age = person.yearOfDeath - person.yearOfBirth;
+    }
+    if (age > oldestAge) {
+      oldestAge = age;
+      oldest = person;
+    }
+    
+    return oldest;
+  }, {});
+
+  return oldestPerson;
 };
 
 // Do not edit below this line
