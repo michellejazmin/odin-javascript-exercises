0165f22007b83ef09d91e7012e9c1e0e5743511b
Michelle Jazmín Perna <76192627+michellejazmin@users.noreply.github.com>
2022-10-12 21:10:57 -0300

    Exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  const sequence = [1, 1];
+  
+  const number = Number(num);
+  if (number < 1 || isNaN(number)) return "OOPS";
 
+  for (let i = 2; i < number; i++) {
+    const newNumber = sequence[i - 2] + sequence[i - 1];
+    sequence.push(newNumber);
+  }
+  
+  return sequence[number - 1];
 };
 
 // Do not edit below this line
